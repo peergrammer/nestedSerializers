d83a0b6571c3d7a7dfc73ef57eef00df7588a53f
peergrammer <phdwight@gmail.com>
2020-11-01 10:10:37 +0800

    Enabling Basic Authentication
    Enabling basic authentication

--- nsApp/views.py
+++ nsApp/views.py
@@ -3,7 +3,8 @@ from .models import Author, Book
 from nsApp.serializer import AuthorSerializer, BookSerializer
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class BookListPagination(LimitOffsetPagination):
     page_size = 2
@@ -11,6 +12,8 @@ class BookListPagination(LimitOffsetPagination):
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
